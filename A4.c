d3ffa25ab5c0938d456dd172ddd6901a4afa1db7
debaratigithyb <ghoshdebarati057@gmail.com>
2025-03-04 20:19:49 +0530

    Add files via upload

---
+++ A4.c
@@ -0,0 +1,17 @@
+#include<stdio.h>
+int main()
+{
+	int a;
+	scanf("%d",a);
+	printf("1%d\n",a,a*1);
+	printf("2%d\n",a,a*2);
+	printf("3%d\n",a,a*3);
+	printf("4%d\n",a,a*4);
+	printf("5%d\n",a,a*5);
+	printf("6%d\n",a,a*6);
+	printf("7%d\n",a,a*7);
+	printf("8%d\n",a,a*8);
+	printf("9%d\n",a,a*9);
+	printf("10%d\n",a,a*10);
+	return 0;
+}
